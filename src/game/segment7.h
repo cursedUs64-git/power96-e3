6bb5e6b2d5a9c80ecc526f734e43d74002456797
cursedUs64-git <realdude691213@gmail.com>
2024-04-29 18:48:34 +0530

    Init, use qol-sm64 fork. Port some things from the old repo

---
+++ src/game/segment7.h
@@ -0,0 +1,27 @@
+#ifndef SEGMENT7_H
+#define SEGMENT7_H
+
+#include <PR/ultratypes.h>
+#include <PR/gbi.h>
+
+// from main menu segment 7
+extern u8 dl_menu_idle_hand[];
+extern u8 dl_menu_grabbing_hand[];
+extern u8 menu_hud_lut[];
+extern u8 menu_font_lut[];
+extern u8 dl_menu_ia8_text_begin[];
+extern u8 dl_menu_ia8_text_end[];
+extern u8 dl_menu_rgba16_wood_course[];
+#ifdef VERSION_EU
+extern u8 dl_menu_rgba16_wood_course_end[];
+extern u8 dl_menu_texture_course_upper[];
+extern u8 dl_menu_texture_niveau_upper[];
+extern u8 dl_menu_texture_kurs_upper[];
+
+extern const u8 eu_course_strings_en_table[];
+extern const u8 eu_course_strings_fr_table[];
+extern const u8 eu_course_strings_de_table[];
+#endif
+
+#include "levels/intro/header.h"
+#endif // SEGMENT7_H
